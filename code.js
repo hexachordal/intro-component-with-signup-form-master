562e532644b1a2a71827a07dc23aa437b50ed859
hexachordal <francois.harewood@gmail.com>
2021-08-29 18:04:17 -0500

    commit on master

--- code.js
+++ code.js
@@ -1,12 +1,38 @@
 console.clear();
 
-/*
-let valid = document.getElementsByClassName('valid');
 
-for(let x= 0; x < valid.length; x++){
-    valid[x].onclick= function hideText(){
-        valid[x].placeholder = "";
+let formItem = document.getElementsByClassName('form-item');
+let button = document.getElementsByClassName('button');
+let invalid = document.getElementsByClassName('invalid');
+
+    formItem[0].onkeypress= function typing(){
+        if(invalid[0].style.color === "rgb(255, 122, 122)"){
+            invalid[0].style.color = "white";
+            formItem[0].style.backgroundImage = "none"
+        }
     }
-}
-*/
+
+    formItem[1].onkeypress= function typing(){
+        if(invalid[1].style.color === "rgb(255, 122, 122)"){
+            invalid[1].style.color = "white";
+            formItem[1].style.backgroundImage = "none"
+        }
+    }
+
+    button[0].onclick= function error(){
+        if(formItem[0].value === ""){
+            invalid[0].style.color = "rgb(255, 122, 122)";
+            formItem[0].style.backgroundImage = `url("images/icon-error.svg")`;
+        }
+
+        if(formItem[1].value === ""){
+            invalid[1].style.color = "rgb(255, 122, 122)";
+            formItem[1].style.backgroundImage = `url("images/icon-error.svg")`;
+        }
+
+    }
+
+    
+
+
 
--- form.php
+++
@@ -1,6 +0,0 @@
-<html>
-    <body>
-        Welcome <?php echo htmlspecialchars($_POST["f-name"]); ?><br>
-        We have received your request.
-    </body>
-</html>
